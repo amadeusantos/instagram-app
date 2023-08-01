d412943f9f04d20ada1608b2fb9157a64ccf5b7e
Amadeu Santos <amadeusantos31@gmail.com>
2023-08-01 18:58:54 -0300

    fix: separando a estilização do corpo do TabBar

--- src/components/tabBar/index.tsx
+++ src/components/tabBar/index.tsx
@@ -1,6 +1,7 @@
 import { View } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
 import Perfil from "../perfil";
+import { styles } from "./style";
 
 interface TabBarProps {
     image: string
@@ -8,7 +9,7 @@ interface TabBarProps {
 
 export default function TabBar({ image }: TabBarProps) {
     return (
-        <View style={{ position: "absolute", bottom:"0%", width: "100%", height: 60, flexDirection: "row", backgroundColor: "#fff", alignItems: "center", justifyContent: "space-around" }}>
+        <View style={styles.container}>
             <Ionicons name="home-outline" size={32} color="black" />
             <Ionicons name="search-outline" size={32} color="black" />
             <Ionicons name="add" size={32} color="black" />
---
+++ src/components/tabBar/style.ts
@@ -0,0 +1,5 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+    container: { position: "absolute", bottom:"0%", width: "100%", height: 60, flexDirection: "row", backgroundColor: "#fff", alignItems: "center", justifyContent: "space-around" }
+})
