f68e90f97c1a89d215f45c255817d6c259022078
Amadeu Santos <amadeusantos31@gmail.com>
2023-08-01 19:08:32 -0300

    fix: extraindo o props de Button

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -1,13 +1,8 @@
-import { ReactNode } from "react";
 import { TouchableOpacity } from "react-native";
 import { styles } from "./style";
+import { ButtonProps } from "./ButtonProps";
 
-interface BottonProps {
-    children: ReactNode;
-    action?: () => void;
-}
-
-export default function Button({ children, action }: BottonProps) {
+export default function Button({ children, action }: ButtonProps) {
     return(
         <TouchableOpacity activeOpacity={0.6} style={styles.padding4} onPress={action} >
             {children}
